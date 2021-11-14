c448c60569f1c5187887fe54f800f0a29a9e5d2d
keith223 <zorinbanan@gmail.com>
2021-11-14 14:53:34 +1100

    изменил свой файл и добавил свой js

---
+++ js/zorbrith22.js
@@ -0,0 +1,8 @@
+// $("button").click(function(){
+    // alert(1212334);
+//     $(".r1").toggle();
+//   });
+$('#but').on('click', function() {
+    alert(5874524354);
+    // $(".popupGames").toggle();
+});

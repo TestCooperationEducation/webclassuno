f76dee8018e0d2165e5d2022684b06afa195cc3d
goredfs <lotuspowerbank@gmail.com>
2022-04-03 14:03:32 +1100

    ид

--- js/leonid.js
+++ js/leonid.js
@@ -1,329 +1,196 @@
+  
+  var tmp = new Object();
+  receiveTexts();
   $('#toggleBirds').on('click', function() {
     $(".popupBirds").toggle();
-    receiveTexts();
+    applyTexts(tmp, "toggleBirds");
   });
   
   $('#closeBirds').on('click', function() {
     $(".popupBirds").hide();
-    receiveTexts()
   });
 
+  receiveTexts();
   $('#toggleDescenders').on('click', function() {
     $(".popupDescenders").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleDescenders");
   });
   
   $('#closeDescenders').on('click', function() {
     $(".popupDescenders").hide();
-    receiveTexts()
   });
 
+  receiveTexts();
   $('#toggleRiders').on('click', function() {
     $(".popupRiders").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleRiders");
   });
   
   $('#closeRiders').on('click', function() {
     $(".popupRiders").hide();
-    receiveTexts()
   });
   
+  receiveTexts();
   $('#toggleBear').on('click', function() {
     $(".popupBear").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleBear");
   });
   
   $('#closeBear').on('click', function() {
     $(".popupBear").hide();
-    receiveTexts()
   });
 
+  receiveTexts();
   $('#toggleWB').on('click', function() {
     $(".popupWB").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleWB");
   });
   
   $('#closeWB').on('click', function() {
     $(".popupWB").hide();
-    receiveTexts()
   });
  
+  receiveTexts();
   $('#toggleSoviet').on('click', function() {
     $(".popupSoviet").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleSoviet");
   });
   
   $('#closeSoviet').on('click', function() {
     $(".popupSoviet").hide();
-    receiveTexts()
   });
 
+  receiveTexts();
   $('#togglePizza').on('click', function() {
     $(".popupPizza").toggle();
-    receiveTexts()
+    applyTexts(tmp, "togglePizza");
   });
 
   $('#closePizza').on('click', function() {
     $(".popupPizza").toggle();
-    receiveTexts()
   });
 
+  receiveTexts();
   $('#toggleMcn').on('click', function() {
     $(".popupMcn").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleMcn");
   });
 
   $('#closeMcn').on('click', function() {
     $(".popupMcn").toggle();
-    receiveTexts()
   });
  
+  receiveTexts();
   $('#toggleSus').on('click', function() {
     $(".popupSus").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleSus");
   });
  
   $('#closeSus').on('click', function() {
     $(".popupSus").toggle();
-    receiveTexts()
   });
  
+  receiveTexts();
   $('#toggleAbout').on('click', function() {
     $(".popupAbout").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleAbout");
   });
  
   $('#closeAbout').on('click', function() {
     $(".popupAbout").toggle();
-    receiveTexts()
   });
  
+  receiveTexts();
   $('#toggleSchool').on('click', function() {
     $(".popupSchool").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleSchool");;
   });
  
   $('#closeSchool').on('click', function() {
     $(".popupSchool").toggle();
-    receiveTexts()
   });
- 
+
+  receiveTexts();
   $('#toggleHob').on('click', function() {
     $(".popupHob").toggle();
-    receiveTexts()
+    applyTexts(tmp, "toggleHob");;
   });
  
   $('#closeHob').on('click', function() {
     $(".popupHob").toggle();
-    receiveTexts()
   });
  
 function receiveTexts() {
-  var tmp = new Object();
     $.post('../php/indexLeonid.php', {}, async function(data) {
       try {
-          tmp = await JSON.parse(data);
-          // alert(tmp[0].text);
-          // $("#sqOneText").append(tmp[0].text);
-          applyTexts(tmp); 
+          tmp = await JSON.parse(data);          
         } catch(e) {
           console.log(e);
         }   
     });         
 }
 
-function applyTexts(data) {
+function applyTexts(data, id) {
   for (var i = 0; i < Object.keys(data).length; i++) {
-    if (data[i].textName == "angrybirds") {
+    if (id == "toggleBirds") {
+      if (data[i].textName == "angrybirds") {
         $(".ang1text").append(data[i].text);
+      }
     }
-    if (data[i].textName == "descenders") {
-        $(".desc1text").append(data[i].text);
+    if (id == "toggleDescenders") {
+      if (data[i].textName == "descenders") {
+          $(".desc1text").append(data[i].text);
+      }
     }
-    if (data[i].textName == "ridersrepublic") {
-      $(".rid1text").append(data[i].text);
-    }
-    if (data[i].textName == "masha") {
-      $(".masha1text").append(data[i].text);
-    }
-    if (data[i].textName == "wbkids") {
-      $(".wb1text").append(data[i].text);
-    }
-    if (data[i].textName == "soviet") {
-      $(".sov1text").append(data[i].text);
-    }
-    if (data[i].textName == "pizza") {
-      $(".piz1text").append(data[i].text);
-    }
-    if (data[i].textName == "mac") {
-      $(".mac1text").append(data[i].text);
-    }
-    if (data[i].textName == "sushi") {
-      $(".sus1text").append(data[i].text);
-    }
-    if (data[i].textName == "about") {
-      $(".about1text").append(data[i].text);
-    }
-    if (data[i].textName == "school") {
-      $(".school1text").append(data[i].text);
-    }
-    if (data[i].textName == "hobby") {
-      $(".hob1text").append(data[i].text);
-    }
-  }
-}
-
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    if (id == "toggleRiders") {
       if (data[i].textName == "ridersrepublic") {
-          $(".rid1text").append(data[i].text);
+        $(".rid1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "toggleBear") {
       if (data[i].textName == "masha") {
-          $(".masha1text").append(data[i].text);
+      $(".masha1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "toggleWB") {
       if (data[i].textName == "wbkids") {
-          $(".wb1text").append(data[i].text);
+        $(".wb1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "toggleSoviet") { 
       if (data[i].textName == "soviet") {
-          $(".sov1text").append(data[i].text);
+        $(".sov1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "togglePizza") {
       if (data[i].textName == "pizza") {
-          $(".piz1text").append(data[i].text);
+        $(".piz1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "toggleMcn") {
       if (data[i].textName == "mac") {
-          $(".mac1text").append(data[i].text);
+        $(".mac1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "toggleSus") {
       if (data[i].textName == "sushi") {
-          $(".sus1text").append(data[i].text);
+        $(".sus1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "toggleAbout") {
       if (data[i].textName == "about") {
-          $(".about1text").append(data[i].text);
+        $(".about1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
-      if (data[i].textName == "about") {
-          $(".about1text").append(data[i].text);
-      }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "toggleSchool") {
       if (data[i].textName == "school") {
-          $(".school1text").append(data[i].text);
+        $(".school1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
-  }
-}
-
-function applyTexts(data) {
-  for (var i = 0; i < Object.keys(data).length; i++) {
+    }
+    if (id == "toggleHob") {
       if (data[i].textName == "hobby") {
-          $(".hob1text").append(data[i].text);
+        $(".hob1text").append(data[i].text);
       }
-  //     if (data[i].textName == "poppy") {
-  //         $("#ppOneText").append(data[i].text);
-  //     }
-  //     if (data[i].textName == "playtime") {
-  //         $("#ppTwoText").append(data[i].text);
-  //     }
+    }
   }
 }

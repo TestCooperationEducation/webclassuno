7259a64d4d900001f4242f1f676c66c7f63abe73
DenisSchool <denisdobrov78@gmail.com>
2022-07-24 14:21:53 +1100

    фото

---
+++ js/denis.js
@@ -0,0 +1,40 @@
+$('#darkest').on('click', function() {
+    $(".popupDarkest").toggle();
+    receiveTexts();
+});
+
+$('#desc').on('click', function() {
+    $(".popupDesc").toggle();
+});
+
+$('#riders').on('click', function() {
+    $(".popupRiders").toggle();
+});
+
+$('#closeDarkest').on('click', function() {
+    $(".popupDarkest").hide();
+});
+
+$('#closeDesc').on('click', function() {
+    $(".popupDesc").hide();
+});
+
+$('#closeRiders').on('click', function() {
+    $(".popupRiders").hide();
+});
+
+$('#receiveTexts').on('click', function() {
+    receiveTexts();
+});
+
+function receiveTexts() {
+    var tmp = new Object();
+      $.post('../php/index.php', {}, async function(data) {
+        try {
+            tmp = await JSON.parse(data);
+            alert(tmp[0].Text);
+          } catch(e) {
+            console.log(e);
+          }   
+      });    
+}

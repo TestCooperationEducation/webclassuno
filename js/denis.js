8eb39665d69f4b8981ba7b4383536e9bbe57659d
DenisSchool <denisdobrov78@gmail.com>
2022-07-31 13:55:17 +1100

    Продолжение работы

--- js/denis.js
+++ js/denis.js
@@ -27,6 +27,36 @@ $('#receiveTexts').on('click', function() {
     receiveTexts();
 });
 
+$('#Counter-Strike').on('click', function() {
+    $(".popupDarkest").toggle();
+    receiveTexts();
+});
+
+$('#desc').on('click', function() {
+    $(".popupDesc").toggle();
+});
+
+$('#riders').on('click', function() {
+    $(".popupRiders").toggle();
+});
+
+$('#closeDarkest').on('click', function() {
+    $(".popupDarkest").hide();
+});
+
+$('#closeDesc').on('click', function() {
+    $(".popupDesc").hide();
+});
+
+$('#closeRiders').on('click', function() {
+    $(".popupRiders").hide();
+});
+
+$('#receiveTexts').on('click', function() {
+    receiveTexts();
+});
+
+
 function receiveTexts() {
     var tmp = new Object();
       $.post('../php/index.php', {}, async function(data) {
